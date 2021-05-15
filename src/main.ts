d39e2a95e33cc66eb71cd06058bd5474c857bdbb
Ricardo Boss <contact@ricardoboss.de>
2021-05-15 13:48:52 +0200

    Added generic type argument to plugin creation method

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import {Store} from "vuex";
 export {VesselStateModule, VesselState, VesselStateActions, VesselStateGetters }
 
 // noinspection JSUnusedGlobalSymbols
-export function createPlugin(options: any = {}) {
-	return function<R = any>(store: Store<R>) {
+export function createPlugin<R = any>(options: any = {}) {
+	return function(store: Store<R>) {
 		const namespace = options.namespace || 'vessel';
 
 		store.registerModule(namespace, new VesselStateModule<R>(), options);
